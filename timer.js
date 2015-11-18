a12165f3b65e00261c608950bbbf6a52082a265e
hausen6 <hausen62480@gmail.com>
2015-11-18 18:35:33 +0900

    タイマー自体はおよそ作動するように変更

--- timer.js
+++ timer.js
@@ -3,22 +3,75 @@
 	
 	var startButton = document.getElementById("startButton");
 	var stopButton = document.getElementById("stopButton");
-	var resetButton = document.getElementById("resetButton");
-	var inputHour = document.getElementById("inputHour");
-	var inputMin = document.getElementById("inputMin");
-	var inputSec = document.getElementById("inputSec");
+	var resumeButton = document.getElementById("resumeButton");
+	var Hour = document.getElementById("inputHour");
+	var Min = document.getElementById("inputMin");
+	var Sec = document.getElementById("inputSec");
+	var textTime = document.getElementById("timeText");
 
-	var lastTime = 0.0;
+	var lastTime = 0;
+	var h, m, s;
 
 	var isRunning = false;
+	var timerID;
 
-	function updataTime() {
-		
+	function setButtonState(start, stop, resume){
+		startButton.disabled = !start;
+		stopButton.disabled = !stop;
+		resumeButton.disabled = !resume;
+
+		startButton.className = start ? "button active": "button inactive";
+		stopButton.className  = stop ? "button active": "button inactive";
+		resumeButton.className = resume ? "button active": "button inactive";
 	}
 
+	function updateTime() {
+		timerID = setTimeout(function (){
+			lastTime -= 1;
+			h = parseInt(lastTime / 3600);
+			m = parseInt((lastTime - (3600 * h)) / 60);
+			s = parseInt(lastTime - (3600 * h) - (60 * m));
+			textTime.innerHTML = h + ":" + m + ":" + s;
+			updateTime();
+		}, 1000);
+	}
+
+	// start button を押した際の処理
 	startButton.addEventListener("click", function () {
+		// フラグ管理
 		if (isRunning) return;
 		isRunning = true;
-		lastTime = inputHour.g
+		// entry からの数字読み込み
+		h = parseInt(document.forms.id_form1.inputHour.value);
+		m = parseInt(document.forms.id_form1.inputMin.value);
+		s = parseInt(document.forms.id_form1.inputSec.value);
+		// 時間計算
+		lastTime = h * 3600 + m * 60 + s;
+		textTime.innerHTML = h + ":" + m + ":" + s;
+		// 処理の呼び出し
+		updateTime();
+		// button の状態を変化
+		setButtonState(false, true, false);
 	});
+
+	// stop button 処理
+	stopButton.addEventListener("click", function () {
+		// フラグ管理
+		if (!isRunning) return;
+		isRunning = false;
+		// タイマーの削除
+		clearTimeout(timerID);
+		// button の状態を変化
+		setButtonState(true, false, true);
+	});
+
+	// resume button 処理
+	resumeButton.addEventListener("click", function(){
+		// フラグ管理
+		if (isRunning) return;
+		isRunning = true;
+		updateTime();
+		// button の状態を変化
+		setButtonState(false, true, false);
+	}) 
 })();
